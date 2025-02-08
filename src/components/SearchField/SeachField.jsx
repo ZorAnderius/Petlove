cd21afd1aa2f6b525214778ef95a2553685a1036
ZorAnderius <gabriel.zorander@gmail.com>
2025-02-08 19:04:06 +0000

    add search input for news page

--- src/components/SearchField/SeachField.jsx
+++ src/components/SearchField/SeachField.jsx
@@ -1,11 +1,13 @@
 import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 import Input from '../Input/Input.jsx';
 import Button from '../Button/Button.jsx';
 import Icon from '../Icon/Icon.jsx';
 import styles from './SearchField.module.css';
 
 const SearchField = ({ action }) => {
+  const [isVisible, setIsVisible] = useState(false);
   const { register, handleSubmit } = useForm();
   const dispatch = useDispatch();
 
@@ -13,12 +15,42 @@ const SearchField = ({ action }) => {
     const validData = data?.search?.trim();
     if (validData) dispatch(action(validData));
   };
+
+  const handleClearFilter = () => {
+    dispatch(action(''));
+    setTimeout(() => setIsVisible(false), 0);
+  };
+
+  const handleChange = e => {
+    e?.target?.value?.trim() ? setIsVisible(true) : setIsVisible(false);
+  };
+
   return (
-    <form className={styles} onSubmit={handleSubmit(handleSubmitSearch)}>
-      <Input type="text" label="search" register={register} />
-      <Button type="submit">
-        <Icon name="search" size={18} />
-      </Button>
+    <form
+      className={styles['search-form']}
+      onSubmit={handleSubmit(handleSubmitSearch)}
+      onChange={handleChange}
+    >
+      <Input
+        type="text"
+        label="search"
+        style="search-input"
+        register={register}
+      />
+      <fieldset className={styles['btn-form-container']}>
+        {isVisible && (
+          <Button
+            type="reset"
+            style="clear-btn"
+            handleClick={handleClearFilter}
+          >
+            <Icon name="cross-small" size={18} />
+          </Button>
+        )}
+        <Button type="submit" style="search-btn">
+          <Icon name="search" size={18} />
+        </Button>
+      </fieldset>
     </form>
   );
 };
