4cf9f936dc161f83a9042a3fa361506bcd1d41f2
ZorAnderius <gabriel.zorander@gmail.com>
2025-02-19 19:20:00 +0000

    fix: pagination in news page

--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -1,40 +1,20 @@
 import NewsItem from '../NewsItem/NewsItem.jsx';
-import Pagination from '../Pagination/Pagination.jsx';
-import { useDispatch, useSelector } from 'react-redux';
-import { setPage } from '../../redux/news/slice.js';
-import {
-  selectCurrentPage,
-  selectNews,
-  selectTotalPage,
-} from '../../redux/news/selector.js';
+import { useSelector } from 'react-redux';
+import { selectNews } from '../../redux/news/selector.js';
 import styles from './NewsList.module.css';
 
-const NewsList = ({ style }) => {
-  const dispatch = useDispatch();
+const NewsList = () => {
   const news = useSelector(selectNews);
-  const page = useSelector(selectCurrentPage);
-  const totalPages = useSelector(selectTotalPage);
-
-  const handlePage = value => {
-    dispatch(setPage(value));
-  };
 
   return (
-    <div className={styles[style]}>
-      <ul className={styles['news-list']}>
-        {!!news &&
-          news.map(newItem => (
-            <li key={newItem._id} className={styles['news-item']}>
-              <NewsItem newItem={newItem} />
-            </li>
-          ))}
-      </ul>
-      <Pagination
-        totalPages={totalPages}
-        page={page}
-        handlePage={handlePage}
-      />
-    </div>
+    <ul className={styles['news-list']}>
+      {!!news &&
+        news.map(newItem => (
+          <li key={newItem._id} className={styles['news-item']}>
+            <NewsItem newItem={newItem} />
+          </li>
+        ))}
+    </ul>
   );
 };
 
--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -6,20 +6,27 @@ import Container from '../../components/Container/Container.jsx';
 import Title from '../../components/Title/Title.jsx';
 import SearchField from '../../components/SearchField/SeachField.jsx';
 import NewsList from '../../components/NewsList/NewsList.jsx';
-import { getFilterValue } from '../../redux/news/slice.js';
+import { getFilterValue, setPage } from '../../redux/news/slice.js';
 import styles from './NewsPage.module.css';
 import {
   selectCurrentPage,
   selectFilterValue,
   selectIsEmpty,
+  selectTotalPage,
 } from '../../redux/news/selector.js';
 import NotFound from '../../components/NotFound/NotFound.jsx';
+import Pagination from '../../components/Pagination/Pagination.jsx';
 
 const NewsPage = () => {
   const dispatch = useDispatch();
   const page = useSelector(selectCurrentPage);
+  const totalPages = useSelector(selectTotalPage);
   const filterValue = useSelector(selectFilterValue);
   const isEmpty = useSelector(selectIsEmpty);
+
+  const handlePage = value => {
+    dispatch(setPage(value));
+  };
 
   useEffect(() => {
     dispatch(getNews({ page, filterValue }));
@@ -32,7 +39,18 @@ const NewsPage = () => {
           <div className={styles['news-container']}>
             <Title style="news-title">News</Title>
             <SearchField style="news_search" action={getFilterValue} />
-            {isEmpty ? <NewsList style="news-container" /> : <NotFound />}
+            {isEmpty ? (
+              <div className={styles['list-wrap']}>
+                <NewsList />
+                <Pagination
+                  totalPages={totalPages}
+                  page={page}
+                  handlePage={handlePage}
+                />
+              </div>
+            ) : (
+              <NotFound />
+            )}
           </div>
         </Container>
       </Section>
